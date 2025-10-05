5fd264618203873021e4ac57f94f014483508557
rbfcdog <rodrigobfcdog@gmail.com>
2025-10-05 18:14:33 -0300

    dog

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 import app from './app.js';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
